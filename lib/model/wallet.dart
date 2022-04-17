0278b13a9e45b634f75f0eba9f599f43bbb3c413
Vishal Menon <vishalmenon.92@gmail.com>
2022-04-17 13:58:35 +0530

    Updated to sdk >2.16.0

--- lib/model/wallet.dart
+++ lib/model/wallet.dart
@@ -5,14 +5,16 @@ import 'package:sats/model/transaction.dart';
 part 'wallet.g.dart';
 part 'wallet.freezed.dart';
 
+const satsInBTC = 100000000;
+
 @freezed
 class Wallet with _$Wallet {
   @HiveType(typeId: 1, adapterName: 'WalletClassAdaper')
   const factory Wallet({
     @HiveField(0) required String label,
-    @HiveField(1) required InternalWallet mainWallet,
-    @HiveField(2) required InternalWallet exportWallet,
-    @HiveField(3) InternalWallet? backedupWallet,
+    @HiveField(1) required String descriptor,
+    @HiveField(2) required int requiredPolicyElements,
+    @HiveField(2) required List<String> policyElements,
     @HiveField(4) required String blockchain,
     @HiveField(5) List<Transaction>? transactions,
     @HiveField(6) int? id,
@@ -25,24 +27,16 @@ class Wallet with _$Wallet {
   factory Wallet.fromJson(Map<String, dynamic> json) => _$WalletFromJson(json);
 
   String balanceToBtc() =>
-      balance == null ? '0' : (balance! / 100000000).toStringAsFixed(8);
+      balance == null ? '0' : (balance! / satsInBTC).toStringAsFixed(8);
 
   bool isNotWatchOnly() => label != 'WATCH ONLY';
 }
 
-@freezed
-class InternalWallet with _$InternalWallet {
-  @HiveType(typeId: 2, adapterName: 'InternalWalletClassAdaper')
-  const factory InternalWallet({
-    @HiveField(0) String? xPriv,
-    @HiveField(1) required String xPub,
-    @HiveField(2) required String fingerPrint,
-    @HiveField(3) required String path,
-    @HiveField(4) String? descriptor,
-    @HiveField(5) DateTime? rescueDate,
-  }) = _InternalWallet;
-  const InternalWallet._();
-
-  factory InternalWallet.fromJson(Map<String, dynamic> json) =>
-      _$InternalWalletFromJson(json);
+enum policyElements {
+  publicKey("[fingerprint/path]xkey"),
+  timeLock("7894564"),
+  hashLock("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+  
+  const policyElements(this.value);
+  final String value;
 }
