52d048633033e2cb3a207a1d383612c8b1a63423
Vishal Menon <vishalmenon.92@gmail.com>
2022-05-11 16:30:15 +0530

    Updated tor test to also include isRunning:true State

--- test/lib/cubit/tor_test.dart
+++ test/lib/cubit/tor_test.dart
@@ -23,11 +23,16 @@ void main() {
       act: (cubit) async {
         torCubit.start();
         torCubit.checkStatus();
+        // we need to sleep and checkStatus after 10 seconds
+        // the line below will break the test
+        // await Future.delayed(Duration(seconds: 10));
+        torCubit.checkStatus();
         torCubit.stop();
       },
       expect: () => <TorState>[
         // const TorState(port: 9150, isRunning: false),
         // const TorState(port: 9150, isRunning: false),
+        // const TorState(port: 9150, isRunning: true),
         // const TorState(port: 9150, isRunning: false),
       ],
     );
