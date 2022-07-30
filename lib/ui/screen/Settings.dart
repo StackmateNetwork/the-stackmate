c03cef5403b60c47ba2754f9d8a505951f60cdc0
yashwanth <yashwanth.bnb@gmail.com>
2022-07-30 19:43:46 +0530

    settings page scroll

--- lib/ui/screen/Settings.dart
+++ lib/ui/screen/Settings.dart
@@ -18,49 +18,51 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 24),
-            Header(
-              cornerTitle: 'Settings'.toUpperCase(),
-              children: [
-                Back(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 24),
+              Header(
+                cornerTitle: 'Settings'.toUpperCase(),
+                children: [
+                  Back(
+                    onPressed: () {
+                      Navigator.of(c).pop();
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const SelectNode(),
+              const SizedBox(height: 8),
+              const SelectNetwork(),
+              const SizedBox(height: 8),
+              const SetBitcoinUnit(),
+              const SizedBox(height: 8),
+              const SetIncognito(),
+              const SizedBox(height: 8),
+              const TorSettings(),
+              const SizedBox(height: 8),
+              SizedBox(
+                height: 52,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: c.colours.primary,
+                    onPrimary: c.colours.background,
+                  ),
                   onPressed: () {
+                    c.read<MasterKeyCubit>().init();
+                    c.read<WalletsCubit>().refresh();
                     Navigator.of(c).pop();
                   },
+                  child: const Text('SAVE'),
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            const SelectNode(),
-            const SizedBox(height: 8),
-            const SelectNetwork(),
-            const SizedBox(height: 8),
-            const SetBitcoinUnit(),
-            const SizedBox(height: 8),
-            const SetIncognito(),
-            const SizedBox(height: 8),
-            const TorSettings(),
-            const SizedBox(height: 8),
-            SizedBox(
-              height: 52,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: c.colours.primary,
-                  onPrimary: c.colours.background,
-                ),
-                onPressed: () {
-                  c.read<MasterKeyCubit>().init();
-                  c.read<WalletsCubit>().refresh();
-                  Navigator.of(c).pop();
-                },
-                child: const Text('SAVE'),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
