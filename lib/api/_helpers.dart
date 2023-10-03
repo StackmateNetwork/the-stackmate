f67c460b6f76ffdc3156830fbc92c916f263b619
yashwanth <yashwanth.bnb@gmail.com>
2023-10-03 20:13:00 +0530

    Removed dio from pubspec

--- lib/api/_helpers.dart
+++ lib/api/_helpers.dart
@@ -1,31 +1,31 @@
-import 'dart:convert';
-import 'package:dio/dio.dart';
-import 'package:sats/cubit/logger.dart';
-import 'package:sats/pkg/_locator.dart';
+// import 'dart:convert';
+// import 'package:dio/dio.dart';
+// import 'package:sats/cubit/logger.dart';
+// import 'package:sats/pkg/_locator.dart';
 
-final client = Dio(BaseOptions(validateStatus: (status) => true));
+// final client = Dio(BaseOptions(validateStatus: (status) => true));
 
-Response parseResponse(Response res) {
-  try {
-    locator<Logger>().logAPI(
-      res.requestOptions.path,
-      jsonEncode(res.data),
-      res.statusCode!,
-    );
-  } catch (e, s) {
-    locator<Logger>().logException(e, 'api.logresponse', s);
-  }
+// Response parseResponse(Response res) {
+//   try {
+//     locator<Logger>().logAPI(
+//       res.requestOptions.path,
+//       jsonEncode(res.data),
+//       res.statusCode!,
+//     );
+//   } catch (e, s) {
+//     locator<Logger>().logException(e, 'api.logresponse', s);
+//   }
 
-  return res;
-}
+//   return res;
+// }
 
-const timeOut = Duration(seconds: 5);
+// const timeOut = Duration(seconds: 5);
 
-void checkAPIStatus(Response response) {
-  final statusCode = response.statusCode;
-  if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
-    throw 'Unauthorised';
-  }
-  if (statusCode == 500) throw 'Error Occured. Try Again';
-  if (statusCode != 200) throw response.data.toString();
-}
+// void checkAPIStatus(Response response) {
+//   final statusCode = response.statusCode;
+//   if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+//     throw 'Unauthorised';
+//   }
+//   if (statusCode == 500) throw 'Error Occured. Try Again';
+//   if (statusCode != 200) throw response.data.toString();
+// }
